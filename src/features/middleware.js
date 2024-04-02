d711d14c14a1422356cdb4e7f960ab410c431b72
S-Romeaux <sal.4845@gmail.com>
2024-04-01 18:36:42 -0700

    connect to api w/ middleware

--- src/features/middleware.js
+++ src/features/middleware.js
@@ -7,9 +7,3 @@ export const logger = store => next => action => {
         console.log('after dispatch', store.getState())
     }
 }
-
-// export const logger = store => next => action => {
-//     console.log('dispatch', store.getState())
-//     next(action)
-//     console.log('after dispatch', store.getState())
-// }
--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
+
 import { configureStore } from '@reduxjs/toolkit'
-import modeReducer from './features/modeSlice'
+import dataReducer from './features/dataSlice'
+import { logger } from './features/middleware'
 
 export const store = configureStore({
     reducer: {
-        mode: modeReducer
-    }
+        data: dataReducer
+    },
+    middleware: [logger]
 })
